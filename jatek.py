786dfa6d157709e8065461a2854052651017352e
KarolyFinta <fintakarolyarmand@gmail.com>
2025-05-14 13:32:55 +0200

    par + sor

--- jatek.py
+++ jatek.py
@@ -12,7 +12,12 @@ while len(jatekos2) != 2:
 
 
 while jatekos1 >= 21 and jatekos2 >= 21:
-    
+    valasz1 = input("Szeretnél húzni egy kártyát?(jatekos1, igen/nem)")
+    if valasz1 = igen:
+        jatekos1.append(random.randint(2, 11))
+    else:
+        valasz2 = input("Szeretnél húzni egy kártyát?(jatekos2, igen/nem)")
+        
 
 
 print(jatekos1)
